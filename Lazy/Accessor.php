ebae008c04d68bc685aec06e8085a3adeba2a7e1
larfeus <larfeus@gmail.com>
2016-12-19 17:52:30 +0300

    Add lazy traits

---
+++ Lazy/Accessor.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Larfeus\Lazy;
+
+/**
+ * Lazy loading
+ *
+ * Usage:
+ *
+ * 		class City {
+ * 			use Accessor;
+ *
+ * 			public function __get_streets() {
+ *
+ * 				return $mysql->get_list('SELECT `id`, `name` FROM `streets`');
+ * 			}
+ * 		}
+ *
+ * 		$city = new City();
+ * 		$city->streets;
+ *
+ * @author larfeus
+ */
+trait Accesor {
+
+	/**
+	 * Magic getter
+	 *
+	 * @param string $name
+	 * @return mixed
+	 */
+	public function __get($name) {
+
+		if (property_exists($this, $name)) {
+			return $this->{$name};
+		}
+
+		$methodCache = true;
+		$methodName = "__get_{$name}";
+
+		if (!method_exists($this, $methodName)) {
+			$methodCache = false;
+			$methodName = "___get_{$name}";
+		}
+
+		if (method_exists($this, $methodName)) {
+
+			$methodResult = $this->{$methodName}();
+
+			if ($methodCache) {
+				$this->{$name} = $methodResult;
+			}
+
+			return $methodResult;
+		}
+
+		return null;
+	}
+}
---
+++ Lazy/Mutator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Larfeus\Lazy;
+
+/**
+ * Lazy setter
+ *
+ * Usage:
+ *
+ * 		class Street {
+ * 				use Mutator;
+ *
+ * 				protected function __set_city($value) {
+ *
+ * 					$this->city_id = $value->id;
+ *
+ * 					return $value;
+ * 				}
+ * 		}
+ *
+ * @author larfeus
+ */
+trait Mutator {
+
+	/**
+	 * Magic setter
+	 *
+	 * @param string $name
+	 * @param mixed $value
+	 */
+	protected function __set($name, $value) {
+
+		$method = "__set_{$name}";
+
+		if (method_exists($this, $method)) {
+			$value = $this->{$method}($value);
+		}
+
+		$this->{$name} = $value;
+	}
+}
